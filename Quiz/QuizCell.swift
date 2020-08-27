cec949aba88f04edf8dcd04d101b4c9e410e18a0
Andrii Halabuda <andrii.halabuda@gmail.com>
2020-08-27 16:12:35 +0200

    Populating tableView with quizzes from endpoints or db if already initialized

---
+++ Quiz/QuizCell.swift
@@ -0,0 +1,31 @@
+//
+//  QuizCell.swift
+//  Quiz
+//
+//  Created by Andrew on 8/27/20.
+//  Copyright © 2020 Andrii Halabuda. All rights reserved.
+//
+
+import UIKit
+
+class QuizCell: UITableViewCell {
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var statusLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func configureCell(topic: Topic) {
+        titleLbl.text = topic.title
+        statusLbl.text = String(topic.progress)
+    }
+
+}
---
+++ Quiz/QuizListVC.swift
@@ -0,0 +1,111 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Andrew on 8/22/20.
+//  Copyright © 2020 Andrii Halabuda. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class QuizListVC: UIViewController {
+    @IBOutlet weak var quizzesTableView: UITableView!
+    
+    private let spinner = SpinnerViewController()
+    private var topics: [Topic] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        quizzesTableView.delegate = self
+        quizzesTableView.dataSource = self
+        
+        initializeDBFromEndpoint()
+        retrieveDataFromDB()
+    }
+    
+    func retrieveDataFromDB() {
+        if topics.count == 0 {
+            let topics = try! Realm().objects(Topic.self)
+            
+            for topic in topics {
+                self.topics.append(topic)
+            }
+            quizzesTableView.reloadData()
+        }
+    }
+    
+    private func initializeDBFromEndpoint() {
+        if !isInitialized {
+            if ConnectionManager.isConnected() {
+                downloadQuiz()
+            } else {
+                showAlertWithTitle("Connection Error", message: "Wooops:(\nLooks like your network is down")
+                Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
+                    if ConnectionManager.isConnected() {
+                        self.downloadQuiz()
+                        timer.invalidate()
+                    }
+                }
+            }
+        }
+    }
+    
+    private func downloadQuiz() {
+        addSpinner(spinner)
+        DownloadManager.downloadQuiz { ids in
+            ids.forEach { (id) in
+                DownloadManager.downloadTopic(id: id) { topic in
+                    guard let topic = topic else { return }
+                    self.topics.append(topic)
+                    if id == ids[ids.endIndex - 1] {
+                        DispatchQueue.main.async {
+                            self.removeSpinner(self.spinner)
+                            self.quizzesTableView.reloadData()
+                        }
+                    }
+                    DispatchQueue.main.async {
+                        
+                        let realm = try! Realm()
+                        try! realm.write {
+                            realm.add(topic)
+                        }
+                    }
+                }
+            }
+            UserDefaults.standard.set(true, forKey: "isInitialized")
+            UserDefaults.standard.synchronize()
+        }
+    }
+
+}
+
+// MARK: - UITableView delegate and dataSource
+extension QuizListVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return topics.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Selected")
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "QuizCell", for: indexPath) as? QuizCell {
+            let topic = topics[indexPath.row]
+            cell.configureCell(topic: topic)
+            return cell
+        } else {
+            return QuizCell()
+        }
+    }
+}
+
